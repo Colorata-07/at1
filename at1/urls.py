ae02e6bf52a85af60367ed40a5467baa303b39ed
Keira <keira.godfrey@icloud.com>
2024-04-04 17:22:46 +1100

    james

--- at1/urls.py
+++ at1/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include(('users.urls', 'users'), namespace='users')),
-    path('eduprod/', include(('eduprod.urls', 'eduprod'), namespace='eduprod')),
     path('accounts/login/', include('users.urls')),
+    path('eduprod/', include(('eduprod.urls', 'eduprod'), namespace='eduprod')),
 ]
